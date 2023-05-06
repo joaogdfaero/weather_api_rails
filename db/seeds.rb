5fb90bda12aa672f259ed7d2c468c19cd3c7226a
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-05-06 14:07:08 -0300

    feat: api weather last temperature with json

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+l = Location.create(name: "Porto Alegre")
+l.recordings.create(temp: 20, status: "nublado")
+l.recordings.create(temp: 21, status: "nublado")
+l.recordings.create(temp: 19, status: "nublado")
+l.recordings.create(temp: 25, status: "chuvoso")
+l.recordings.create(temp: 28, status: "ensolarado")
